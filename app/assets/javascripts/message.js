66c0e375d39c76dab6fb3a52266c910cfbb67a13
gonzou72 <s.gonzou@gmail.com>
2019-07-28 16:55:42 +0900

    add-auto-finish1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
         var image = ""
         message.image ? image = `<img src="${message.image}">` : image = ""
 
-        var html =`<div class="message">
+        var html =`<div class="message" data-message-id="${message.id}">
                       <div class="upper-message">
                         <div class="upper-message__user-name">
                          ${message.user_name}
@@ -41,6 +41,7 @@ $(function(){
         var html = buildHTML(user_new_message);
         $('.messages').append(html);
         $('.content').val('');
+        $('#new_message')[0].reset();
         $('.form__submit').prop('disabled', false);
         scroll()
     })
@@ -49,4 +50,28 @@ $(function(){
         $('.form__submit').prop('disabled', false)
     })
   })
+  
+  var reloadMessages = function() {
+      last_message_id = $('.message').last().data("message-id");
+    $.ajax({
+      url: 'api/messages',
+      type: 'GET',
+      dataType: 'json',
+      data: {last_id: last_message_id}
+      })
+
+    .done(function(messages) {
+      messages.forEach(function(message){
+        var insertHTML = buildHTML(message)     
+      $('.messages').append(insertHTML)
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 200);
+        })
+      
+    })
+
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 });
