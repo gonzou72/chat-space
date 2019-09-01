55e35e13e7f2a61d00f39d35d1804003e6e41af2
gonzou72 <s.gonzou@gmail.com>
2019-09-01 23:05:08 +0900

    change-bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory = 'acuo'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/acuo'
+  config.fog_directory = 'fruitflavors'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fruitflavors'
 end
