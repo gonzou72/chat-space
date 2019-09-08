da526c9f78433a9525432da98accafba2621dd63
gonzou72 <s.gonzou@gmail.com>
2019-09-08 21:57:05 +0900

    fix-coding-by-rubocop

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -45,17 +45,17 @@ describe MessagesController do
 
       context 'can save' do
         subject {
-            post :create,
-                 params: params
+          post :create,
+               params: params
         }
 
         it 'count up message' do
-            expect{ subject }.to change(Message, :count).by(1)
+          expect{ subject }.to change(Message, :count).by(1)
         end
 
         it 'redirects to group_messages_path' do
-            subject
-            expect(response).to redirect_to(group_messages_path(group))
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
         end
       end
 
@@ -63,27 +63,27 @@ describe MessagesController do
         let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
 
         subject{
-            post :create,
-                 params: invalid_params
+          post :create,
+               params: invalid_params
         }
 
         it 'does not count up' do
-            expect{ subject }.not_to change(Message, :count)
+          expect{ subject }.not_to change(Message, :count)
         end
 
         it 'renders index' do
-            subject
-            expect(response).to render_template :index
+          subject
+          expect(response).to render_template :index
         end
       end
     end
 
     context 'not log in' do
         
-        it 'redirects to new_user_session_path' do
-            post :create, params: params
-            expect(response).to redirect_to(new_user_session_path)
-        end
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
